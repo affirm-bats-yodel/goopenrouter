7805fe7892ed7a6df07f11ef326168d44e612e7c
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-15 10:28:31 +0900

    feat: add error status code

--- error.go
+++ error.go
@@ -36,10 +36,56 @@ func (e *ErrorResponse) Error() string {
 
 var _ error = (*ErrorResponse)(nil)
 
+// StatusCode OpenRouter Status Code
+//
+// refer from OpenRouter's Errors Page
+//
+// https://openrouter.ai/docs/errors#error-codes
+type StatusCode int
+
+const (
+	// StatusBadRequest Bad Request
+	//
+	// Invalid or missing params, CORS
+	StatusBadRequest StatusCode = 400
+	// StatusInvalidCredentials Invalid credentials
+	//
+	// OAuth session expired, disabled/invalid API key
+	StatusInvalidCredentials StatusCode = 401
+	// StatusInsufficientCredit Insufficient Credit
+	//
+	// Your account or API key has insufficient credits.
+	// Add more credits and retry the request.
+	StatusInsufficientCredit StatusCode = 402
+	// StatusFlagged Input Flagged
+	//
+	// Your chosen model requires moderation
+	// and your input was flagged
+	StatusFlagged StatusCode = 403
+	// StatusTimedout Timed Out
+	//
+	// Your request timed out
+	StatusTimedout StatusCode = 408
+	// StatusRateLimited Rate Limited
+	//
+	// You are being rate limited
+	StatusRateLimited StatusCode = 429
+	// StatusInvalidResponse Invalid Response from Provider
+	//
+	// Your chosen model is down
+	// or we received an invalid response from it
+	StatusInvalidResponse StatusCode = 502
+	// StatusUnavailable Model Unavailable
+	//
+	// There is no available model provider that
+	// meets your routing requirements
+	StatusUnavailable StatusCode = 503
+)
+
 // ErrorDetail Error Detail
 type ErrorDetail struct {
 	// Code Error Code
-	Code int `json:"code"`
+	Code StatusCode `json:"code"`
 	// Message Error Message
 	Message string `json:"message"`
 }
