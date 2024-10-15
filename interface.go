26f57e269257b65d3398344299a0118859922f73
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-15 11:51:10 +0900

    feat: add result type of GetModels

--- interface.go
+++ interface.go
@@ -7,7 +7,7 @@ import "context"
 // - Define a functions to implement
 type ClientInterface interface {
 	// GetModels Get Model Information
-	GetModels(ctx context.Context, parameters ...string)
+	GetModels(ctx context.Context, parameters ...string) ([]*Model, error)
 	// GetLimits Get Limit Information
 	GetLimits(ctx context.Context) (*Limit, error)
 }
