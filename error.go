18534bc75b10ecc770869141c8e1de7e5a7e35d0
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-15 11:28:50 +0900

    fix: remove ErrorResponse, add Error to ErrorDetail
      * ErrorResponse does not required anymore
      because Generic Response struct has error.

--- error.go
+++ error.go
@@ -2,40 +2,6 @@ package openrouter
 
 import "fmt"
 
-// IsErrorResponse Check error is ErrorResponse
-func IsErrorResponse(e error) bool {
-	if e == nil {
-		return false
-	}
-	_, b := e.(*ErrorResponse)
-	return b
-}
-
-// ErrorResponse Error Response from OpenRouter
-//
-// Compatible with Golang's Error interface.
-//
-// For Example:
-//
-//	{"error": {"code": 0, "message": "string"}}
-type ErrorResponse struct {
-	ErrorDetail *ErrorDetail `json:"error"`
-}
-
-// UnknownError Error Message when there is no
-// message (or if ErrorDetail is Empty)
-const UnknownError = "error: unknown error occurred"
-
-// Error implements error.
-func (e *ErrorResponse) Error() string {
-	if e.ErrorDetail != nil && e.ErrorDetail.Message != "" {
-		return fmt.Sprintf("error: (%d): %s", e.ErrorDetail.Code, e.ErrorDetail.Message)
-	}
-	return UnknownError
-}
-
-var _ error = (*ErrorResponse)(nil)
-
 // StatusCode OpenRouter Status Code
 //
 // refer from OpenRouter's Errors Page
@@ -89,3 +55,10 @@ type ErrorDetail struct {
 	// Message Error Message
 	Message string `json:"message"`
 }
+
+// Error implements error.
+func (e *ErrorDetail) Error() string {
+	return fmt.Sprintf("error (%d): %s", e.Code, e.Message)
+}
+
+var _ error = (*ErrorDetail)(nil)
