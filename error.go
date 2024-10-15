25f4b1389ac77782ab6c1d5389ca237358c7e356
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-15 10:11:24 +0900

    feat: add error model

---
+++ error.go
@@ -0,0 +1,36 @@
+package openrouter
+
+import "fmt"
+
+// ErrorResponse Error Response from OpenRouter
+//
+// Compatible with Golang's Error interface.
+//
+// For Example:
+//
+//	{"error": {"code": 0, "message": "string"}}
+type ErrorResponse struct {
+	ErrorDetail *ErrorDetail `json:"error"`
+}
+
+// UnknownError Error Message when there is no
+// message (or if ErrorDetail is Empty)
+const UnknownError = "error: unknown error occurred"
+
+// Error implements error.
+func (e *ErrorResponse) Error() string {
+	if e.ErrorDetail != nil && e.ErrorDetail.Message != "" {
+		return fmt.Sprintf("error: (%d): %s", e.ErrorDetail.Code, e.ErrorDetail.Message)
+	}
+	return UnknownError
+}
+
+var _ error = (*ErrorResponse)(nil)
+
+// ErrorDetail Error Detail
+type ErrorDetail struct {
+	// Code Error Code
+	Code int `json:"code"`
+	// Message Error Message
+	Message string `json:"message"`
+}
