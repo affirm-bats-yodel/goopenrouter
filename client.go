48d7cb0b1b16451f7f5c2ea2698fa4d8797842e8
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-15 11:30:17 +0900

    feat: impl GetLimits

---
+++ client.go
@@ -0,0 +1,89 @@
+package openrouter
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+)
+
+// EnvOpenRouterKey a Environment Key for OpenRouter API Key
+const EnvOpenRouterKey = "OPENROUTER_API_KEY"
+
+var ErrEnvNoRouterKey = errors.New("error: no API key exist on $" + EnvOpenRouterKey)
+
+// NewClient Create new OpenRouter Client
+func NewClient() (*Client, error) {
+	v := os.Getenv(EnvOpenRouterKey)
+	if v == "" {
+		return nil, ErrEnvNoRouterKey
+	}
+	return &Client{
+		APIKey: v,
+	}, nil
+}
+
+// Client OpenRouter Client Implementation
+type Client struct {
+	APIKey string
+}
+
+// GetLimits implements ClientInterface.
+func (c *Client) GetLimits(ctx context.Context) (*LimitResponse, error) {
+	var (
+		lr Response[LimitResponse]
+	)
+
+	req, err := newHTTPRequest(ctx, "GET", "/api/v1/auth/key", c.APIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := newHTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if err := json.NewDecoder(res.Body).Decode(&lr); err != nil {
+		return nil, err
+	}
+
+	if err := res.Body.Close(); err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, lr.Error
+	}
+
+	return lr.Data, nil
+}
+
+// GetModels implements ClientInterface.
+func (c *Client) GetModels(ctx context.Context, parameters ...string) {
+	panic("unimplemented")
+}
+
+var _ ClientInterface = (*Client)(nil)
+
+// newHTTPClient Create a new http.Client
+func newHTTPClient() *http.Client {
+	return &http.Client{}
+}
+
+const (
+	openRouterAddr      = "https://openrouter.ai"
+	authorizationHeader = "Authorization"
+)
+
+// newHTTPRequest Create a http.Request
+func newHTTPRequest(ctx context.Context, method string, endpoint string, apiKey string) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, method, openRouterAddr+endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add(authorizationHeader, "Bearer "+apiKey)
+	return req, nil
+}
